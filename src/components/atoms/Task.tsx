b4bdc9d006756d6f255a3da7d5e00ea89fcb7cc5
Henrique Lima <barrosohenriquelima@gmail.com>
2022-11-09 14:25:48 -0300

    imp task

--- src/components/atoms/Task.tsx
+++ src/components/atoms/Task.tsx
@@ -1,9 +1,32 @@
-import styles from './Task.module.css'
+import { CheckCircle, Circle, Trash } from "phosphor-react";
+import styles from "./Task.module.css";
+import { TaskModel } from "../../models/TaskModel";
 
-export function Task(){
-    return (
-        <div>
-            Essa é uma tarefa
-        </div>
-    )
+export function Task({
+  id,
+  text,
+  taskStatus,
+  onChangeTaskStatus,
+  onDeleteTask,
+}: TaskModel): JSX.Element {
+ 
+  return (
+    <div className={styles.task}>
+      <button className={styles.checkButton} onClick={() => onChangeTaskStatus(id)}>
+        {taskStatus === "done" ? (
+          <CheckCircle size={24} weight="fill" className={styles.check} />
+        ) : (
+          <Circle size={24} className={styles.check} />
+        )}
+      </button>
+      <p>{text}</p>
+      <button
+        title="Excluir"
+        className={styles.delete}
+        onClick={ () => onDeleteTask(id)}
+      >
+        <Trash size={24} />
+      </button>
+    </div>
+  );
 }
